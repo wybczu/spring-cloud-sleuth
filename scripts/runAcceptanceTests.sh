2f6b4e75481d15da78cbcdeafce0afbb78b87f68
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2015-12-10 00:36:20 +0100

    Added acceptance tests for sleuth

---
+++ scripts/runAcceptanceTests.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+curl https://raw.githubusercontent.com/spring-cloud-samples/brewery/master/acceptance-tests/scripts/runDockerAcceptanceTests.sh --output runDockerAcceptanceTests.sh
+sh runDockerAcceptanceTests.sh -DWHAT_TO_TEST=SLEUTH
